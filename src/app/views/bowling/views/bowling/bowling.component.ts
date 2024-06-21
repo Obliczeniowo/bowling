9dcd5a6e6d74019411c64b2791a9235691519006
obliczeniowo <zajaczkowskikrzysztofkz@gmail.com>
2024-06-21 12:14:22 +0200

    add score form

--- src/app/views/bowling/views/bowling/bowling.component.ts
+++ src/app/views/bowling/views/bowling/bowling.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RoundsModel } from './models/rounds.model';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-bowling',
@@ -8,4 +9,34 @@ import { RoundsModel } from './models/rounds.model';
 })
 export class BowlingComponent {
   scores = new RoundsModel();
+
+  form!: FormGroup;
+
+  translationgs: {[error: string]: string} = {
+    max: "Maximum points is 10",
+    min: "Minimum points is 0",
+    required: "Value not set or unproper"
+  }
+
+  constructor(fb: FormBuilder) {
+    this.form = fb.group(
+      {
+        'first': [ null, [Validators.required, Validators.min(0), Validators.max(10)]],
+        'second': [ null, [Validators.required, Validators.min(0), Validators.max(10)]]
+      }
+    )
+  }
+
+  add() {
+    if (this.form.valid) {
+      this.scores.add(this.form.getRawValue());
+      this.form.setValue({ first: null, second: null});
+    } else {
+      this.form.markAllAsTouched();
+    }
+  }
+
+  reset() {
+    this.scores.setEmpty();
+  }
 }
