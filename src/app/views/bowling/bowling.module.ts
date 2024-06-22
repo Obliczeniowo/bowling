d64dbb86acd2874393a98dc132285de97d50cf86
obliczeniowo <zajaczkowskikrzysztofkz@gmail.com>
2024-06-22 09:30:36 +0200

    remove imports

--- src/app/views/bowling/bowling.module.ts
+++ src/app/views/bowling/bowling.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { InputModule } from '@obliczeniowo/elementary/input';
 import { ButtonsModule } from '@obliczeniowo/elementary/buttons';
 
 import { BowlingRoutingModule } from './bowling-routing.module';
 import { BowlingComponent } from './views/bowling/bowling.component';
 import { NavigationComponent } from './views/navigation/navigation.component';
-import { ReactiveFormsModule } from '@angular/forms';
 import { ControlsModule } from '../../controls/controls.module';
 
 
